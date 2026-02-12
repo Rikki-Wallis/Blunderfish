ddba400a3acc131afced76c32113ea1842e49aa6
Jeremy Lim <jlim0233@student.monash.edu>
2026-02-13 00:55:09 +1100

    Start of CLI

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ static size_t select_move(const std::unordered_map<std::string, size_t>& moves) 
     }
 }
 
-int main() {
+static int play_main() {
     std::string start_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"; 
 
     auto maybe_pos = Position::decode_fen_string(start_fen);
@@ -91,4 +91,48 @@ int main() {
     */
 
     return 0;
+}
+
+static int eval_main(const char* FEN) {
+    (void)FEN;
+    printf("Position eval: 67\n");
+    return 0;
+}
+
+static int best_main(const char* FEN) {
+    (void)FEN;
+    printf("Best move: bruh!\n");
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        printf("Usage: %s <play, eval, best> <FEN?>\n", argv[0]);
+        return 1;
+    }
+
+    if (strcmp(argv[1], "play") == 0) {
+        return play_main();
+    }
+
+    if (strcmp(argv[1], "eval") == 0) {
+        if (argc < 3) {
+            printf("Usage: %s eval <FEN>\n", argv[0]);
+            return 1;
+        }
+
+        return eval_main(argv[2]);
+    }
+
+    if (strcmp(argv[1], "best") == 0) {
+        if (argc < 3) {
+            printf("Usage: %s best <FEN>\n", argv[0]);
+            return 1;
+        }
+
+        return best_main(argv[2]);
+    }
+
+    printf("Invalid mode given '%s'\n", argv[1]);
+    return 1;
 }
